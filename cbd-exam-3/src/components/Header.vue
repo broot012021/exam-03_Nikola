265b8b1e5d1a9546b6978a3fb46c018d2588cfdf
bevzenj <brisk0110@gmail.com>
2021-08-18 03:06:59 +0300

    initial commit

---
+++ cbd-exam-3/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+<header class="app-header">
+    <nav class="app-header-nav">
+
+    </nav>
+    <div class="app-header-logo">
+
+    </div>
+    <div class="app-header-tools">
+
+    </div>
+</header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss">
+
+</style>
