4a58d898d520e4c06e3a7298f7f186308cbe4d8c
bevzenj <brisk0110@gmail.com>
2021-09-01 19:04:26 +0300

    create products slider

---
+++ cbd-exam-3/src/components/Product1.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="product">
+      <div class="product__rating">
+        &#9733;
+        &#9733;
+      </div>
+      <h3 class="product__title">
+        Black ICE CBD Muscle Rub 200 mg
+      </h3>
+      <img src="~@/assets/img/products/product-1.png"
+      alt="product" class="product__img">
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Product1',
+};
+
+</script>
+
+<style lang="scss">
+
+</style>
