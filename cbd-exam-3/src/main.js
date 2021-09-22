9f2d203274ffa61935b2cf3e6df039a2f85c8ba1
bevzenj <brisk0110@gmail.com>
2021-09-22 21:22:09 +0300

    finish faq

--- cbd-exam-3/src/main.js
+++ cbd-exam-3/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
+import '@/assets/styles/general.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/assets/styles/fonts.scss';
-import '@/assets/styles/general.scss';
 
 Vue.config.productionTip = false;
 
